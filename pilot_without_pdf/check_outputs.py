5663eda7d3c0b2b935e7848b7a4247d919d9c3bf
sroh722 <sroh722@gmail.com>
2026-02-25 17:07:13 +0800

    Update check_outputs.py

--- pilot_without_pdf/check_outputs.py
+++ pilot_without_pdf/check_outputs.py
@@ -5,10 +5,3 @@ pd.set_option("display.max_columns", None)
 
 df = pd.read_csv("data/test_extraction.csv")
 print(df)
-
-
-import src.batch_config
-dir(src.batch_config)
-
-import src.batch_config
-print(src.batch_config.__file__)
